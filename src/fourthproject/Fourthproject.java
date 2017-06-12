bf9e79d9c717a495b188aa3319bbe335882f9e64
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-06-12 19:54:36 +0300

    Switch-case

--- src/fourthproject/Fourthproject.java
+++ src/fourthproject/Fourthproject.java
@@ -5,6 +5,8 @@
  */
 package fourthproject;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Boro_s99
@@ -16,21 +18,56 @@ public class Fourthproject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int firstValue = 0;
+        System.out.println("Task 1");
+        Scanner in = new Scanner(System.in); 
+        System.out.println("Please enter weekday number. Week starts at Sunday");
+        firstValue = in.nextInt();  
         
-        switch ('C')
+        switch (firstValue)
         {
-            case 'A':
-                System.out.println("OK1");
+            case 1:
+                System.out.println("Sunday");
                 break;
-                
-            case 'B':
-                System.out.println("OK2");
+            case 2:
+                System.out.println("Monday");
                 break;
-            
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wendnesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;    
             default:
-                System.out.println("OK3");
+                System.out.println("Saturday");
         }
         
+        
+        System.out.println("Task 2");
+        int temp1 = 0;
+        System.out.println("Currency exchange");
+        System.out.println("Please enter amount in hryvnas");
+        firstValue = in.nextInt();  
+        System.out.println("Please enter 1 for dollars, 2 for roubles, 3 for euros");
+        temp1 = in.nextInt();
+        switch (temp1) {
+            case 1:
+                System.out.println("Now you have " + firstValue / 25.95 + " dollars");
+                break;
+            case 2:
+                System.out.println("Now you have " + firstValue / 0.456955 + " roubles");
+                break;
+            case 3:
+                System.out.println("Now you have " + firstValue / 29.19 + " euros");
+                break;
+            
+        }
     }
     
 }

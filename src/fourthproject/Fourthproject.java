90476c796bf8de447276aeac96d5a1628787c396
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-06-12 18:44:50 +0300

    Switch-case

---
+++ src/fourthproject/Fourthproject.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fourthproject;
+
+/**
+ *
+ * @author Boro_s99
+ */
+public class Fourthproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        switch ('C')
+        {
+            case 'A':
+                System.out.println("OK1");
+                break;
+                
+            case 'B':
+                System.out.println("OK2");
+                break;
+            
+            default:
+                System.out.println("OK3");
+        }
+        
+    }
+    
+}
